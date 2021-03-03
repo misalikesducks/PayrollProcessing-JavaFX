955db01edccc5255e21c945f9564e2f23b53bcc8
Connie Chen <conne.chen1101@users.noreply.github.com>
2021-03-03 17:09:26 -0500

    hi test scene

--- src/PayrollProcessing/Controller.java
+++ src/PayrollProcessing/Controller.java
@@ -1,4 +1,39 @@
 package PayrollProcessing;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.scene.control.Alert.AlertType;
+import java.io.File;
+
 public class Controller {
+
+   @FXML
+   private TextField empName, hours, partRate;
+   @FXML
+   private DatePicker dateHired;
+   @FXML
+   private TextArea show1;
+   @FXML
+   void get(ActionEvent event) {
+      try {
+         Date day = new Date("11/01/2000");
+         Profile prof = new Profile(empName.getText(), "IT", day);
+         show1.appendText(prof.toString());
+      }
+      catch(NumberFormatException e) {
+         Alert alert = new Alert(AlertType.WARNING);
+         alert.setTitle("WARNING!");
+         alert.setHeaderText("YOURE A POO");
+         alert.setContentText("HAHAHHAHAHA idk what im doing ");
+         alert.showAndWait();
+      }
+   }
+
 }
--- src/PayrollProcessing/Main.java
+++ src/PayrollProcessing/Main.java
@@ -4,16 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource( "View.fxml"));
-        primaryStage.setTitle("Life is poop");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource( "View.fxml"));
+            primaryStage.setTitle("Payroll Processing");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
